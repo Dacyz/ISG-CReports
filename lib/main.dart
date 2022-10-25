19c2e7542265724b15914203359ee48ff9f28e2c
Dacyz <dacygaming@gmail.com>
2022-10-25 14:40:40 -0500

    Correciones

--- lib/main.dart
+++ lib/main.dart
@@ -37,62 +37,63 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 0),
-            child: SvgPicture.asset(
-              "assets/icons/InsergeSVGM.svg",
-              color: Colors.black,
-            ),
-          ),
-          const SizedBox(height: defaultPadding),
-          RawMaterialButton(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(defaultBorderRadius)),
-            fillColor: Colors.blueAccent,
-            onPressed: () async {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => login(),
-                ),
-              );
-            },
-            child: const Padding(
-              padding: EdgeInsets.all(defaultPadding),
-              child: Text(
-                "Iniciar Sesión",
-                style: TextStyle(color: Colors.white),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 0),
+              child: SvgPicture.asset(
+                "assets/icons/InsergeSVGM.svg",
+                color: Colors.black,
               ),
             ),
-          ),
-          const SizedBox(height: defaultPadding),
-          RawMaterialButton(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(defaultBorderRadius)),
-            fillColor: Colors.blueAccent,
-            onPressed: () async {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const VisualizatorPage(),
+            const SizedBox(height: defaultPadding),
+            RawMaterialButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(defaultBorderRadius)),
+              fillColor: Colors.blueAccent,
+              onPressed: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => login(),
+                  ),
+                );
+              },
+              child: const Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Text(
+                  "Iniciar Sesión",
+                  style: TextStyle(color: Colors.white),
                 ),
-              );
-            },
-            child: const Padding(
-              padding: EdgeInsets.all(defaultPadding),
-              child: Text(
-                "Catálogo",
-                style: TextStyle(color: Colors.white),
               ),
             ),
-          ),
-      ],
-    ),
-        ));
+            const SizedBox(height: defaultPadding),
+            RawMaterialButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(defaultBorderRadius)),
+              fillColor: Colors.blueAccent,
+              onPressed: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const VisualizatorPage(),
+                  ),
+                );
+              },
+              child: const Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Text(
+                  "Catálogo",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
